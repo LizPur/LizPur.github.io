0335cd167c73f9f7d4cddc470b043ee91cd02d9d
LizPur <shmatov.vladimir07@mail.ru>
2025-11-23 17:56:50 +0300

    'creat'

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let angleradians;
+
 function calculate() {
     let anglenew = document.querySelector('#angle').value;
     if (anglenew) {
@@ -6,21 +7,26 @@ function calculate() {
         alert(`Результат: ${angleradians.toFixed(2)}`);
     }
 }
-function material_m(){
+
+function phusic(){
+   let size = document.querySelector('#size').value;
+   let gravity;
    let select = document.querySelector('select');
    let mater = select.value;
-   if (mater === 'copper'){
-        alert(angleradians * 1000)
-   }
-   if (mater === 'silver'){
-        alert(angleradians * 0)
-   }
-   if (mater === 'iron'){
-        alert(angleradians * 1)
-   }
+   if (mater === 'copper') {
+        gravity = (size * 8.92 * 10)/1000;
+        alert(gravity + ' H');
+}  else if (mater === 'silver') {
+        gravity = (size * 10.49 * 10)/1000;
+        alert(gravity + ' H');
+}  else if (mater === 'iron') {
+        gravity = (size * 7.87 * 10)/1000;
+        alert(gravity + ' H');
+    }
 }
+
 window.onload = () => {
   const button = document.querySelector('#phusicbutton');
   button.addEventListener('click', calculate);
-  button.addEventListener('click', material_m);
+  button.addEventListener('click', phusic);
 };
